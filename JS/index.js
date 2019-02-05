e253572dec4abe4ad18dcd4417d240f6a088279a
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-05 13:30:28 -0500

    Added border around feature tabs/cards. Feature image fades in instead of entire tab/card.

--- JS/index.js
+++ JS/index.js
@@ -19,41 +19,57 @@
 class TabLink {
   constructor(tabElement) {
     this.tabElement = tabElement;
-    // Matches active card to active tab:
+    // Matches active card (and active card image) to active tab:
     this.tabData = this.tabElement.dataset.tab;
     this.card = document.querySelector(
       `.feature-card[data-card="${this.tabData}"]`
     );
+    this.image = document.querySelector(
+      `.feature-image[data-image="${this.tabData}"]`
+    );
+    // Applies inactive borderBottom style to all tabs:
+    this.tabElement.style.borderBottom = "1px solid #080c0d";
     // Applies "click" event listener to all tabs:
     tabElement.addEventListener("click", () => this.selectTab());
   }
   selectTab() {
     // GSAP: Changes all tabs (and thereby the active tab) to inactive instantly
     tabs.forEach(tab =>
-      TweenMax.to(tab, 0, { backgroundColor: "#263b4a", color: "#bdba79" })
+      TweenMax.to(tab, 0, {
+        backgroundColor: "#263b4a",
+        color: "#bdba79",
+        borderBottom: "1px solid #080c0d"
+      })
     );
-    // GSAP: Hides previously active card instantly
+    // GSAP: Hides previously active card and card image instantly
     cards.forEach(card =>
       TweenMax.to(card, 0, { display: "none", opacity: 0 })
     );
-    // GSAP: Fades in active tab/card
-    TweenMax.to(this.tabElement, 0.3, {
+    images.forEach(image => TweenMax.to(image, 0, { opacity: 0 }));
+    // GSAP: Styles active tab/card instantly
+    TweenMax.to(this.tabElement, 0, {
       backgroundColor: "#679ec7",
-      color: "#fffca3"
+      color: "#fffca3",
+      borderBottom: "none"
     });
-    TweenMax.to(this.card, 0.3, { display: "block", opacity: 1 });
+    TweenMax.to(this.card, 0, { display: "block", opacity: 1 });
     // Rounds upper-left corner of feature cards 2 and 3:
     if (this.tabData === `feature 2` || this.tabData === `feature 3`) {
       TweenMax.to(this.card, 0.3, { borderTopLeftRadius: "6px" });
     }
+    // Fades in image on active tab
+    TweenMax.to(this.image, 0.5, { opacity: 1 });
   }
 }
 
 const tabs = document.querySelectorAll(".feature-tab");
 const cards = document.querySelectorAll(".feature-card");
+const images = document.querySelectorAll(".feature-image");
 tabs.forEach(tabElement => new TabLink(tabElement));
 
 // Applies styles to features tab/card that is active by default:
 tabs[0].style.backgroundColor = "#679ec7";
 tabs[0].style.color = "#fffca3";
+tabs[0].style.borderBottom = "none";
 cards[0].style.display = "block";
+images[0].style.opacity = 1;
